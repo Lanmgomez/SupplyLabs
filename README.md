9c1d0980c74853b546fbed8269a84ca7b8b9d52d
islan <islan_gomes@hotmail.com>
2023-12-09 16:08:43 -0300

    Criação e Consumo da API REST

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 npm installs:
 
--npm i antd -D sass
+-npm i antd -D sass axios json-server
+
+Para rodar a API no Json-Server:
+-npm run backend
+
+Para rodar o projeto com o pacote vite:
+-npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import './Sass/App.sass'
+import axios from "axios"
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [apiData, setApiData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/users')
+    .then(response => { setApiData(response.data), console.log(response.data) })
+    .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div className='teste'>
-      <h1>teste</h1>
-      <h2>teste</h2>
-      <h3>teste</h3>
+    <div className='container'>
+      {apiData.map((dataUsers) => (
+        <div key={dataUsers.id}>
+          <ul className='ul-cards-info'>
+            <li>
+              <span>Id:</span> 
+              <li>{dataUsers.id}</li>
+            </li>
+            <li>
+              <span>Nome:</span> 
+              <li>{dataUsers.name}</li>
+            </li>
+            <li>
+              <span>Sobrenome:</span> 
+              <li>{dataUsers.username}</li>
+            </li>
+            <li>
+              <span>Email:</span> 
+              <li>{dataUsers.email}</li>
+            </li>
+            <li>
+              <span>Idade:</span> 
+              <li>{dataUsers.age}</li>
+            </li>
+            <li><button>Ações</button></li>
+          </ul>
+        </div>
+      ))}
     </div>
   )
 }
